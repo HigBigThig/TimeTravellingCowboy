a27267de53d381d15616e25fa2b7d4c867f339af
nbpj0 <nbpj0@schls.albco>
2025-02-07 15:49:03 -0500

    Switched to a physics based jump for CJ
    uses forces and stuff

--- Time Travelling Cowboy/Assets/Scripts/CJ/CJ Movement/CJBasicMovement.cs
+++ Time Travelling Cowboy/Assets/Scripts/CJ/CJ Movement/CJBasicMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CJBasicMovement : MonoBehaviour
 {
 
-
+    public Rigidbody2D rb;
     public Vector3 Velocity = new Vector3(0, 0, 0);
 
     // Start is called before the first frame update
@@ -38,30 +38,17 @@ public class CJBasicMovement : MonoBehaviour
 
         }
 
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, LayerMask.GetMask("Environment"));
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
-            Velocity.y = 10;
+            rb.AddForce(Vector2.up * 500);
 
         }
-        else if (hit.collider == null)
-        {
 
-            Velocity.y -= 10 * Time.deltaTime;
-
-        }
-        else
-        {
-
-            Velocity.y = 0;
-        
-        } 
 
       
 
-        transform.position = transform.position + Velocity * Time.deltaTime;
+        transform.position += Velocity * Time.deltaTime;
 
     }
 }
